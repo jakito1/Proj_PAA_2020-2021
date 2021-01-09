d22fbd4fb463d19b7633255bee827dbf7e8bedda
Francisco <23082107+jakito1@users.noreply.github.com>
2021-01-09 22:41:14 +0000

    Update Caretaker.java

--- src/com/pa/proj2020/adts/graph/Caretaker.java
+++ src/com/pa/proj2020/adts/graph/Caretaker.java
@@ -3,5 +3,20 @@ package com.pa.proj2020.adts.graph;
 public class Caretaker {
     private SocialNetwork socialNetwork;
     private Memento undo;
-    private Memento redo;
+
+    public Caretaker(SocialNetwork socialNetwork){
+        this.socialNetwork = socialNetwork;
+    }
+
+    public void saveState(){
+        undo = socialNetwork.createMemento();
+    }
+
+    public void restoreState() throws NoMementoException {
+        if (undo == null){
+            throw new NoMementoException("Nothing to undo.");
+        }
+
+        socialNetwork.setMemento(undo);
+    }
 }
